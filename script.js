d55541b0a2cbd2b35581cf8e2e4ea90a8c8df038
Chris <pcc386@nyu.edu>
2021-11-16 22:47:11 -0500

    assignment

---
+++ script.js
@@ -0,0 +1,44 @@
+const app = {
+  initialize: function () {
+    app.getData();
+  },
+  getData: function () {
+    console.log("GETTING DATA");
+    const nyURl =
+      "https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json?api-key=";
+    let nyKey = "zofoKJ5CztJjD8YU4MAA88he8HrwnFso";
+    const finalURL = nyURl + nyKey;
+    console.log(finalURL);
+    fetch(finalURL)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        for (let i = 0; i < data.results.length; i++) {
+          console.log(
+            data.results[i].title,
+            data.results[i].author,
+            data.results[i].description
+          );
+        }
+        app.appendData(data);
+      })
+      .catch((error) => console.log(error));
+  },
+  appendData: function (data) {
+    let content = document.querySelector(".booksDiv");
+    console.log("here!");
+    for (let i = 0; i < data.results.length; i++) {
+      let newDiv = document.createElement("div");
+      newDiv.classList.add("bookDiv");
+      let newHeader = document.createElement("h3");
+      newHeader.innerText = i + 1 + ". " + data.results[i].title;
+      let newText = document.createElement("p");
+      newText.innerText = data.results[i].description;
+      let authorText = document.createElement("p");
+      authorText.innerText = "Author: " + data.results[i].author;
+      newDiv.append(newHeader, newText, authorText);
+
+      content.append(newDiv);
+    }
+  },
+};
